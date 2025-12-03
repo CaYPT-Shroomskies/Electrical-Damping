482b103f50073a1e5b618c97668c901576460346
Luyu Wu <luyuwuvk@gmail.com>
2025-12-03 01:05:37 -0500

    yes

--- main.py
+++ main.py
@@ -6,31 +6,44 @@ from scipy.integrate import solve_ivp  # noqa
 # Dipole model, assume small angle across area of coil
 # Given the motion is axial, the dipole equation simplifies to:
 # B (tesla) = (u_0/4pi) * (2u/r^3)
+#
+# NOTE: The dipole approximation asymptotes as the magnet passes through the coil.
+# This is not the case, so we enforce a minimum "distance"
+# This causes a discontinuity in the current graph
+
 pi: float = np.pi
 u_0: float = (10**-7) * (4 * pi)
 mass: float = 0.05  # kg
 m: float = 0.6  # dipole moment
 k: float = 10
 radius: float = 0.02  # Assume same radius for magnet coil
-r: float = 50  # ohms
-N = 1  # number of coils
-L: float = u_0 * pi * radius**2 * 10**2 / 0.02  # Inductance of the coil V = L (dI/dt)
-
-initial: np.ndarray = np.array([0.1, 0, 0])  # position, velocity, I
-runtime = 2
+r: float = 20  # ohms
+N = 40  # number of coils
+L: float = u_0 * pi * radius**2 * N**2 / 0.01  # Inductance of the coil
+initial: np.ndarray = np.array([0.05, 0, 0])  # position, velocity, I
+runtime = 1
+dipole_threshold = 0.01  # where to stop the dipole asymptoting
 
 
 def step(t, state):
     x, x_dot, I = state
-    V: float = (pi * radius**2 * N) * (1e-7) * 2 * m * x_dot * 3 / (x**4)
-    # voltage is dPhi
+    V: float = (
+        (pi * radius**2 * N)
+        * (1e-7)
+        * 2
+        * m
+        * x_dot
+        * 3
+        / max(x, dipole_threshold) ** 4
+    )
+    # emf is dPhi
     m_coil = I * N * pi * radius**2
-    F_b = (10**-7) * 6 * m_coil * m / x**4
-
+    print(m_coil)
+    F_b = (10**-7) * 6 * m_coil * m / max(x, dipole_threshold) ** 4
     return np.array([x_dot, -(k * x + F_b) / m, (V - I * r) / L])
 
 
-time_array = np.linspace(0, runtime, int(runtime * 60))
+time_array = np.linspace(0, runtime, int(runtime * 120))
 returned = solve_ivp(
     step,
     [0, runtime],
@@ -41,10 +54,16 @@ returned = solve_ivp(
 
 solve = returned.y.T
 
-fig, (ax0, ax1) = plt.subplots(2)
+fig, (ax0, ax1, ax2) = plt.subplots(3)
 ax0.plot(time_array, solve[:, 0], label="Position")
-ax1.plot(time_array, solve[:, 2], label="Current")
-ax1.set_ylabel("Current (A)")
+ax1.plot(time_array, solve[:, 1], label="Velocity")
+ax2.plot(time_array, solve[:, 2], label="Current")
+ax2.set_ylabel("Current (A)")
+ax1.set_ylabel("Velocity (m/s)")
 ax0.set_ylabel("Position (m)")
-ax1.set_xlabel("Time (s)")
+ax2.set_xlabel("Time (s)")
+ax0.grid()
+ax1.grid()
+ax2.grid()
+plt.tight_layout()
 plt.show()
